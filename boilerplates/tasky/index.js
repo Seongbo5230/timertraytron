c50aba9907bec03b749c51f41fc5c5667c907dcf
Seong Chang <seongbo5230@gmail.com>
2020-08-11 23:56:25 -0700

    moving tray to timer tray

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const electron = require("electron");
+const TimerTray = require("./app/timer_tray");
 
 const { app, BrowserWindow, Tray } = electron;
 
@@ -19,10 +20,14 @@ app.on("ready", () => {
   const iconName =
     process.platform === "darwin" ? "iconTemplate.png" : "windows-icon.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  tray = new Tray(iconPath);
+
+  tray = new TimerTray(iconPath);
   tray.on("click", (event, bounds) => {
     // Click event bounds
-    const { x, y } = process.platform === "linux" ? electron.screen.getCursorScreenPoint() : bounds;
+    const { x, y } =
+      process.platform === "linux"
+        ? electron.screen.getCursorScreenPoint()
+        : bounds;
 
     // Window height and width
     const { height, width } = mainWindow.getBounds();
@@ -35,7 +40,7 @@ app.on("ready", () => {
         x: x - width / 2,
         y: yPosition,
         height: height,
-        width: width
+        width: width,
       });
       mainWindow.show();
     }
