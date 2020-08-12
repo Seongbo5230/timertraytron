d1dc2b9ed0fe02075103bc79e6ab657daa2dc7cc
Seong Chang <seongbo5230@gmail.com>
2020-08-12 00:39:24 -0700

    oop done

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -5,7 +5,6 @@ const TimerTray = require("./app/timer_tray");
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
-let tray;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({
@@ -20,29 +19,5 @@ app.on("ready", () => {
   const iconName =
     process.platform === "darwin" ? "iconTemplate.png" : "windows-icon.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-
-  tray = new TimerTray(iconPath);
-  tray.on("click", (event, bounds) => {
-    // Click event bounds
-    const { x, y } =
-      process.platform === "linux"
-        ? electron.screen.getCursorScreenPoint()
-        : bounds;
-
-    // Window height and width
-    const { height, width } = mainWindow.getBounds();
-
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      const yPosition = process.platfrom === "darwin" ? y : y - height;
-      mainWindow.setBounds({
-        x: x - width / 2,
-        y: yPosition,
-        height: height,
-        width: width,
-      });
-      mainWindow.show();
-    }
-  });
+  new TimerTray(iconPath, mainWindow);
 });
